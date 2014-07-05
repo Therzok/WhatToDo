eb484c7f25715b5094f4f0027929a1c75c96d165
Marius Ungureanu <marius.ungureanu@xamarin.com>
2014-07-05 22:59:31 +0300

    Initial import.

---
+++ QuestData.lua
@@ -0,0 +1,61 @@
+--[[
+	TODO: MinLevel
+	TODO: Professions
+	TODO: Gather remaining quest ID.
+	TODO: Locale
+]]
+
+require "PlayerPathLib"
+
+-- Faster typed enums.
+local Explorer = PlayerPathLib.PlayerPathType_Explorer
+local Scientist = PlayerPathLib.PlayerPathType_Scientist
+local Settler = PlayerPathLib.PlayerPathType_Settler
+local Soldier = PlayerPathLib.PlayerPathType_Soldier
+
+local WTD = Apollo.GetAddon("WhatToDo")
+WTD.QuestData = {
+	-- Reputation dailies
+	["Crimson Badlands"] = {
+		{ IdD = 7468, IdE = 7464, Name = "HOLDOUT: Stop the Scouts", Path = Soldier },
+		{ IdD = 7466, IdE = 7462, Name = "ANALYSIS: Malicious Mutagen", Path = Scientist },
+		{ IdD = 7469, IdE = 7465, Name = "OPERATIONS: Covert Monitoring", Path = Explorer },
+		{ IdD = 7457, IdE = 7454, Name = "Queen For A Day [GROUP 2+]" },
+		{ IdD = 7592, IdE = 7591, Name = "Unexpected Discovery [GROUP 2+]" },
+		{ IdD = 7448, IdE = 7449, Name = "Scout the Island" },
+		{ IdD = 7456, IdE = 7453, Name = "Slash And Burn" },
+		{ IdD = 7481, IdE = 7484, Name = "Scarhide Extermination" },
+		{ IdD = 7444, IdE = 7445, Name = "Persistent Problems" },
+		{ IdD = 7510, IdE = 7509, Name = "Eater of Dust" },
+		{ IdD = 7573, IdE = 7572, Name = "Boulders to Pebbles" },
+		{ IdD = 7467, IdE = 7463, Name = "Guarding the Front" },
+		{ IdD = 7446, IdE = 7447, Name = "BBQ Blast" },
+		{ IdD = 7458, IdE = 7455, Name = "Burning Waters" },
+		-- Different Quests
+		{ IdD = 7496, IdE = 7486, Name = "D: Reclaim the Power -- E: Equipment Under Siege" },
+		{ IdD = 7498, IdE = 7488, Name = "D: Missing Technologies -- E: Electrical Disturbance" },
+	},
+	["Northern Wastes"] = {
+		{ IdD = 7105, IdE = 7104, Name = "OPERATIONS: Maintaining Communications", Path = Explorer },
+		{ IdD = 7092, IdE = 7086, Name = "ANALYSIS: Crystal Healing", Path = Scientist },
+		{ IdD = 7102, IdE = 7095, Name = "HOLDOUT: Stolen Supplies", Path = Soldier },
+		{ IdD = 7081, IdE = 7082, Name = "Frozen Assets" },
+		{ IdD = 7090, IdE = 7091, Name = "Timely Observations" },
+		{ IdD = 7094, IdE = 7093, Name = "Stocking Up" },
+		{ IdD = 7083, IdE = 7084, Name = "Moodie Mash" },
+		{ IdD = 7070, IdE = 7071, Name = "Icy Enlightenment" },
+		{ IdD = 7068, IdE = 7069, Name = "Frozen Dinners" },
+		{ IdD = 7107, IdE = 7085, Name = "Establising Base Perimeters" },
+		{ IdD = 7089, IdE = 7087, Name = "Grim Scavenging" },
+		{ IdD = 7103, IdE = 7088, Name = "Rescue Party" },
+		{ IdD = 7442, IdE = 7443, Name = "Supply Drop" },
+		{ IdD = 7079, IdE = 7080, Name = "Disarming the Enemy" },
+		{ IdD = 7113, IdE = 7114, Name = "Tame the Wastes" },
+		{ IdD = 7075, IdE = 7076, Name = "They'll thank You Later" },
+		{ IdD = 7077, IdE = 7078, Name = "Eldan Eradication" },
+		{ IdD = 7072, IdE = 7074, Name = "A New God" },
+	}
+--[[["Outfitter"] = {
+		[] = { IdD = 9604, IdE = 9605 Name = "Pellskinner Blues", Profession = CraftingLib.CodeEnumTradeskill.Outfitter }
+	}]]
+}
---
+++ WhatToDo.lua
@@ -0,0 +1,238 @@
+--[[
+	TODO: UI for 'finishing' quests.
+]]
+--require "CraftingLib"
+require "GameLib"
+require "PlayerPathLib"
+require "os"
+require "table"
+
+local Debug
+
+local WhatToDo = {}
+local GeminiGUI
+
+local function clone(t) -- deep-copy a table
+	if type(t) ~= "table" then return t end
+	local meta = getmetatable(t)
+	local target = {}
+	for k, v in pairs(t) do
+		if type(v) == "table" then
+			target[k] = clone(v)
+		else
+			target[k] = v
+		end
+	end
+	setmetatable(target, meta)
+	return target
+end
+
+local function lastReset()
+	local stamp = os.date("!*t")	-- This is the UTC time.
+	return stamp.hour >= 10 and stamp.yday or stamp.yday - 1
+end
+
+function WhatToDo:ResetDailies()
+	self.cfg = {
+		dailies = clone(self.QuestData),
+		last = lastReset()
+	}
+end
+
+function WhatToDo:KeepWithRequirements()
+	local newDailies = {}
+	for k1, v1 in pairs(self.cfg.dailies) do
+		local newList = {}
+		for _, v2 in ipairs(v1) do
+			if not v2.Path or v2.Path == self.playerPath then
+				table.insert(newList, v2)
+			end
+		end
+
+		if #newList ~= 0 then newDailies[k1] = newList end
+	end
+
+	self.cfg.dailies = newDailies
+end
+
+function WhatToDo:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function WhatToDo:Init()
+	Apollo.RegisterAddon(self, false, "", {"Gemini:GUI-1.0"})
+	self.cfg = {}
+end
+
+function WhatToDo:OnSave(eLevel)
+	if eLevel ~= GameLib.CodeEnumAddonSaveLevel.Character then return nil end
+	return self.cfg
+end
+
+function WhatToDo:OnRestore(eLevel, tData)
+	if eLevel ~= GameLib.CodeEnumAddonSaveLevel.Character then return nil end
+
+	self.cfg = clone(tData)
+
+	-- Test if we're over a daily cycle since last login.
+	if self.cfg.last ~= lastReset() then
+		self:ResetDailies()
+	end
+
+	self:KeepWithRequirements()
+end
+
+function WhatToDo:OnLoad()
+	-- Get dependency packages.
+	GeminiGUI = Apollo.GetPackage("Gemini:GUI-1.0").tPackage
+
+	-- Register slash.
+	Apollo.RegisterSlashCommand("wtd", "OnWhatToDoOn", self)
+	Apollo.RegisterSlashCommand("whattodo", "OnWhatToDoOn", self)
+	Apollo.RegisterSlashCommand("wtdf", "OnWhatToDoFinish", self)
+
+	-- Register event for quest completion
+	Apollo.RegisterEventHandler("QuestStateChanged", "OnQuestStateChanged", self)
+
+	self.playerPath = PlayerPathLib.GetPlayerPathType()
+end
+
+-- Track quest completion.
+function WhatToDo:OnQuestStateChanged(queUpdated, eState)
+	if eState == Quest.QuestState_Accepted then
+		if Debug then Print("Accepted [" .. queUpdated:GetId() .. "]: " .. queUpdated:GetTitle()) end
+	elseif eState == Quest.QuestState_Completed then
+		if Debug then Print("Completed [" .. queUpdated:GetId() .. "]: " .. queUpdated:GetTitle()) end
+		self:FinishQuest(queUpdated:GetId())
+	end
+end
+
+-- Remove the quest from the dailies list.
+function WhatToDo:FinishQuest(iQuestId)
+	local zone, index
+	for k1, v1 in pairs(self.cfg.dailies) do
+		for k2, v2 in ipairs(v1) do
+			if v2.IdD == iQuestId or v2.IdE == iQuestId then
+				zone, index = k1, k2
+				break
+			end
+		end
+	end
+
+	if not zone or not index then return end
+
+	table.remove(self.cfg.dailies[zone], index)
+	if #self.cfg.dailies[zone] == 0 then
+		self.cfg.dailies[zone] = nil
+	end
+
+	self:RedrawTree()
+end
+
+-- Tree with items construction.
+local function CreateTree()
+	return { -- Tree Control
+		Name			= "QuestTree",
+		WidgetType		= "TreeControl",
+		AnchorPoints	= "FILL", -- will be translated to { 0, 0, 1, 1 }
+		VScroll			= true,
+		Events			= {
+			WindowLoad = function(self, wndHandler, wndControl)
+				local items = false
+				for k, v in pairs(self.cfg.dailies) do
+					items = true
+
+					local hParent = 0
+					hParent = wndControl:AddNode(hParent, k)
+					for _, v2 in ipairs(v) do
+						local displayName = Debug and v2.Name .. " [" .. v2.IdD .. "," .. v2.IdE .."]" or v2.Name
+						wndControl:AddNode(hParent, displayName)
+					end
+				end
+				if not items then
+					wndControl:AddNode(0, "You've already done everything today. Congratulations.")
+				end
+			end
+		}
+	}
+end
+
+-- Redraws the tree.
+function WhatToDo:RedrawTree()
+	if not self.wndMain then return end
+
+	local container = self.wndMain:FindChild("QuestWidgetContainer")
+	container:DestroyChildren()
+	GeminiGUI:Create(CreateTree()):GetInstance(self, container)
+end
+
+-- On SlashCommand "/wtdf"
+function WhatToDo:OnWhatToDoFinish(strCmd, strArg)
+	self:FinishQuest(tonumber(strArg))
+end
+
+-- on SlashCommand "/wtd"
+function WhatToDo:OnWhatToDoOn()
+	if self.wndMain then return end
+
+	local tWndDefinition = {
+		Name			= "WhatToDoWindow",
+		Template		= "CRB_TooltipSimple",
+		UseTemplateBG	= true,
+		Picture			= false,
+		Moveable		= true,
+		Border			= true,
+		AnchorCenter	= {500, 460},
+		Escapable		= true,
+
+		Pixies = {
+			{
+				Line			= true,
+				AnchorPoints	= "HFILL", -- will be translated to {0,0,1,0},
+				AnchorOffsets	= {0,30,0,30},
+				Color			= "white",
+			},
+			{
+				Text			= "What To Do - Daily Tracker",
+				Font			= "CRB_HeaderHuge",
+				TextColor		= "xkcdYellow",
+				AnchorPoints	= "HFILL",
+				DT_CENTER		= true,
+				DT_VCENTER		= true,
+				AnchorOffsets	= {0,0,0,20},
+			},
+		},
+
+		Children = {
+			{
+				WidgetType		= "PushButton",
+				AnchorPoints	= "TOPRIGHT", -- will be translated to { 1, 0, 1, 0 }
+				AnchorOffsets	= { -17, -3, 3, 17 },
+				Base			= "CRB_Basekit:kitBtn_Holo_Close",
+				NoClip			= true,
+				Events			= { ButtonSignal = function(_, wndHandler, wndControl) wndControl:GetParent():Close() end, },
+			},
+			{ 
+				Name			= "QuestWidgetContainer", 
+				AnchorPoints	= "FILL", -- will be translated to { 0, 0, 1, 1 }
+				AnchorOffsets	= {0,40,0,0},
+				NoSelection		= true,
+			}
+		},
+
+		Events = {
+			WindowClosed = function(self, wndHandler, wndControl)
+				self.wndMain = nil
+			end
+		}
+	}
+
+	self.wndMain = GeminiGUI:Create(tWndDefinition):GetInstance(self)
+	self:RedrawTree()
+end
+
+local WhatToDoInst = WhatToDo:new()
+WhatToDo:Init()
